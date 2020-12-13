e59065712c70eca68123fca6c823b8ce7f7ebb7e
Kyle Quinlan <61363821+kyle-quinlan@users.noreply.github.com>
2020-12-13 18:19:38 -0500

    Fixed script JS so it doesnt search for the length of a null list

--- 2-Movie-Theater-Booking/script.js
+++ 2-Movie-Theater-Booking/script.js
@@ -34,13 +34,14 @@ function updateSelectedCount() {
 function populateUI() {
     const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
     
-    if(selectedSeats != null & selectedSeats.length > 0) {
-        seats.forEach((seat, index) => {
-            if(selectedSeats.indexOf(index) > -1) {
-                seat.classList.add("selected");
-            }
-        });
-    }
+    if(selectedSeats !== null){
+        if(selectedSeats.length > 0){ 
+            seats.forEach((seat, index) => {
+                if(selectedSeats.indexOf(index) > -1) {
+                    seat.classList.add("selected");
+                }
+            });
+    }}
     
     const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
     if(selectedMovieIndex!=null) {
